88cbc4dbb90e3ce2460503abc917d7723dd9f3a7
BioShuan <ricekent@163.com>
2017-11-02 16:55:02 +0800

    finished pfam filter script

--- RNAseq/lncRNA/filter_nc_pfam.py
+++ RNAseq/lncRNA/filter_nc_pfam.py
@@ -5,12 +5,24 @@ from scipy import stats
 
 
 @click.command()
-@click.option('-c', '--coding_pfam', type=click.Path(exists=True), required=True,
-              help='pfam prediction for coding sequences.')
-@click.option('-n', '--noncoding_pfam', type=click.Path(exists=True), required=True,
-              help='pfam prediction for noncoding sequences.')
-@click.option('-p', '--pfam_hit', type=click.File('w'), required=True,
-              help='valid Pfam domains in transcribed regions')
+@click.option(
+    '-c',
+    '--coding_pfam',
+    type=click.Path(exists=True),
+    required=True,
+    help='pfam prediction for coding sequences.')
+@click.option(
+    '-n',
+    '--noncoding_pfam',
+    type=click.Path(exists=True),
+    required=True,
+    help='pfam prediction for noncoding sequences.')
+@click.option(
+    '-p',
+    '--pfam_hit',
+    type=click.File('w'),
+    required=True,
+    help='valid Pfam domains in transcribed regions')
 def main(coding_pfam, noncoding_pfam, pfam_hit):
     cd_df = pd.read_csv(coding_pfam, header=None, delim_whitespace=True)
     cd_total = len(cd_df)
@@ -20,9 +32,12 @@ def main(coding_pfam, noncoding_pfam, pfam_hit):
     nc_count = Counter(nc_df.loc[:, 5])
     for each_hit in cd_count:
         if each_hit in nc_count:
-            cd_hit, cd_nohit = cd_count[each_hit], cd_total - cd_count[each_hit]
-            nc_hit, nc_nohit = nc_count[each_hit], nc_total - nc_count[each_hit]
-            oddsratio, pvalue = stats.fisher_exact([[cd_hit, cd_nohit], [nc_hit, nc_nohit]])
+            cd_hit, cd_nohit = cd_count[
+                each_hit], cd_total - cd_count[each_hit]
+            nc_hit, nc_nohit = nc_count[
+                each_hit], nc_total - nc_count[each_hit]
+            oddsratio, pvalue = stats.fisher_exact([[cd_hit, cd_nohit],
+                                                    [nc_hit, nc_nohit]])
             if oddsratio > 10 and pvalue < 0.05:
                 pfam_hit.write('{h}\n'.format(h=each_hit))
         else:
