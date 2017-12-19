84647577c7d178f89d08c337cf114f1904942146
BioShuan <ricekent@163.com>
2017-12-19 11:25:22 +0800

    modified the project directory structure

--- RNAseq/lncRNA/classify/test.sh
+++
@@ -1,9 +0,0 @@
-echo start
-date
-
-oms_lncRNA_classify \
-    -m Sus_scrofa.Sscrofa11.1.90.gtf \
-    -l novel.lncrna.can.gtf
-
-date
-echo finished
--- utils/config.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-""" omsTools config module. Holds a single copy of
-config variables to be used across all other modules """
