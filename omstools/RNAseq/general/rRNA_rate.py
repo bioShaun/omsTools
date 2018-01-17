d0ba68e5189011c66bf03fffb5bcc38cff5bf232
Bioshaun <ricekent@163.com>
2018-01-17 04:57:38 -0500

    add rRNA_rate.py to extract rRNA rate in bowtie2 logs

---
+++ omstools/RNAseq/general/rRNA_rate.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import re
+
+if not len(sys.argv) == 4:
+    print 'python ' + sys.argv[0] + ' sample_file mapping_dir summary'
+    sys.exit(0)
+
+sample_file = sys.argv[1]
+mapping_dir = sys.argv[2]
+summary = sys.argv[3]
+
+sample_list = [each.strip().split()[1] for each in open(sample_file)]
+
+summary_info = open(summary, 'w')
+summary_info.write('Sample_ID\trRNA_rate\n')
+for n, each_sample in enumerate(sample_list):
+    each_sample_mapping_file = os.path.join(mapping_dir,
+                                            '%s.log' % each_sample)
+    with open(each_sample_mapping_file) as each_sample_mapping_file_info:
+        for eachline in each_sample_mapping_file_info:
+            if 'overall alignment rate' in eachline:
+                mapping_rate = re.search(r'(.*)%', eachline).groups()[0]
+                summary_info.write('%s\t%s\n' % (each_sample, mapping_rate))
+summary_info.close()
