4b95c055bd7c229544bd0b9d18d5a8953f0cd729
Bioshaun <ricekent@163.com>
2018-06-07 22:59:00 -0400

    output more detailed stats and summary

--- omstools/RNAseq/lncRNA/filter/filter_nc_pfam_v2.py
+++ omstools/RNAseq/lncRNA/filter/filter_nc_pfam_v2.py
@@ -3,6 +3,8 @@ from collections import Counter
 import click
 from scipy import stats
 import glob
+import os
+import math
 
 
 def read_split_pfam(split_pfam_dir):
@@ -36,30 +38,74 @@ def read_split_pfam(split_pfam_dir):
     required=True,
     help='pfam prediction for noncoding sequences.')
 @click.option(
-    '-p',
-    '--pfam_hit',
-    type=click.File('w'),
+    '-o',
+    '--pfam_out',
+    type=click.Path(file_okay=False),
     required=True,
-    help='valid Pfam domains in transcribed regions')
-def main(coding_pfam, noncoding_pfam, pfam_hit):
+    help='Pfam analysis output directory.')
+def main(coding_pfam, noncoding_pfam, pfam_out):
     cd_df = read_split_pfam(coding_pfam)
     cd_total = len(cd_df)
+    cd_pfam = len(cd_df.loc[:, 5].unique())
     cd_count = Counter(cd_df.loc[:, 5])
     nc_df = read_split_pfam(noncoding_pfam)
     nc_total = len(nc_df)
+    nc_pfam = len(nc_df.loc[:, 5].unique())
     nc_count = Counter(nc_df.loc[:, 5])
+    valid_pfam_list = list()
+    artif_pfam_list = list()
+    pfam_hit_file = os.path.join(pfam_out, 'valid.pfam')
+    pfam_hit = open(pfam_hit_file, 'w')
+    pfam_plot_file = os.path.join(pfam_out, 'pfam.detail.txt')
+    pfam_plot = open(pfam_plot_file, 'w')
+    pfam_plot.write(
+        "pfam\tcoding_region_hit\tnon-transcribed_region_hit\tother_domain_coding_region_hit\tother_domain_non-transcribed_region_hit\todds_ratio\tpvalue\t\tstatus\n")
     for each_hit in cd_count:
+        cd_hit, cd_nohit = cd_count[
+            each_hit], cd_total - cd_count[each_hit]
+        is_valid = 'Valid Pfam domains'
         if each_hit in nc_count:
-            cd_hit, cd_nohit = cd_count[
-                each_hit], cd_total - cd_count[each_hit]
             nc_hit, nc_nohit = nc_count[
                 each_hit], nc_total - nc_count[each_hit]
-            oddsratio, pvalue = stats.fisher_exact([[cd_hit, cd_nohit],
-                                                    [nc_hit, nc_nohit]])
-            if oddsratio > 10 and pvalue < 0.05:
-                pfam_hit.write('{h}\n'.format(h=each_hit))
         else:
+            nc_hit = 0
+            nc_nohit = nc_total
+        oddsratio, pvalue = stats.fisher_exact([[cd_hit, nc_hit],
+                                                [cd_nohit, nc_nohit]])
+        # if math.isinf(oddsratio):
+        #     pvalue = 0
+        if oddsratio > 10 and pvalue < 0.05:
+            valid_pfam_list.append(each_hit)
             pfam_hit.write('{h}\n'.format(h=each_hit))
+        else:
+            artif_pfam_list.append(each_hit)
+            is_valid = 'Artifact Pfam domains'
+        pfam_plot.write('{hit}\t{ch}\t{nh}\t{cn}\t{nn}\t\t{od}\t{pv}\t{vl}\n'.format(
+            hit=each_hit, ch=cd_hit,
+            nh=nc_hit, vl=is_valid,
+            cn=cd_nohit, nn=nc_nohit,
+            od=oddsratio, pv=pvalue
+        ))
+    pfam_hit.close()
+    pfam_plot.close()
+    valid_pfam_num = len(valid_pfam_list)
+    artif_pfam_num = len(artif_pfam_list)
+    valid_pfam_hits = len(cd_df[cd_df.loc[:, 5].isin(valid_pfam_list)])
+    artif_pfam_hits = cd_total - valid_pfam_hits
+    nc_valid_hits = len(nc_df[nc_df.loc[:, 5].isin(valid_pfam_list)])
+    print '''
+Coding region pfam hits:             {ch}
+Coding region pfam domains:          {cd}
+Non-transcribed region pfam hits:    {nh}
+Non-transcribed region pfam domains: {nd}
+Valid pfam domains:                  {vd}
+Artifacts pfam domains:              {ad}
+Valid pfam hits:                     {vh}
+Artifacts pfam hits:                 {ah}
+Non-transcribed region valid pfam hits: {nvh}
+'''.format(ch=cd_total, cd=cd_pfam, nh=nc_total, nd=nc_pfam,
+           vd=valid_pfam_num, ad=artif_pfam_num, vh=valid_pfam_hits,
+           ah=artif_pfam_hits, nvh=nc_valid_hits)
 
 
 if __name__ == '__main__':
